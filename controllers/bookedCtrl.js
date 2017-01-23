b847d33a05752204f6cc078ed0b938056de60f85
Ryan Clark <Rymaster95@gmail.com>
2017-01-23 16:29:12 -0700

    finished routing project

---
+++ controllers/bookedCtrl.js
@@ -0,0 +1,21 @@
+angular.module("devmtnTravel")
+.controller('bookedCtrl', function($scope, mainSrv, $stateParams) {
+
+var booking = function() {
+for (var i = 0; i < mainSrv.travelInfo.length; i++) {
+if (parseInt($stateParams.id) === mainSrv.travelInfo[i].id) {
+   $scope.location = mainSrv.travelInfo[i];
+    console.log(mainSrv.travelInfo);
+ }
+}
+}
+
+booking()
+
+$scope.style = {
+  'background': 'url('+$scope.location.image+') no-repeat center'
+
+}
+  console.log($scope.background);
+
+})
--- js/app.js
+++ js/app.js
@@ -4,8 +4,37 @@ angular.module('devmtnTravel', ['ui.router'])
             .state('home',{
                 url:'/',
                 templateUrl: "../views/about.html"
-            });
-
+            })
+            .state('packages', {
+              url:'/packages',
+              controller: 'packagesCtrl',
+              templateUrl: "../views/packages.html"
+            })
+            .state('locations', {
+              url:'/locations',
+              controller: 'locationsCtrl',
+              templateUrl: "../views/locations.html"
+            })
+            .state('booked', {
+              url:'/booked/:id',
+              controller: 'bookedCtrl',
+              templateUrl: "../views/booked.html"
+            })
+            .state('adventurers', {
+              url:'/adventurers',
+              parent:'home',
+              templateUrl: "../views/about-adventurers.html"
+            })
+            .state('contact', {
+              url:'/contact',
+              parent:'home',
+              templateUrl: "../views/contact.html"
+            })
+            .state('about', {
+              url:'/about',
+              parent:'home',
+              templateUrl: "../views/about.html"
+            })
         $urlRouterProvider
             .otherwise('/');
     });
